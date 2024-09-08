eb974f28913a037431eb9166c2420ae510695a4c
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-08 20:50:50 +0200

    Update register.js
    added handling on register function. Created a different function for getting the form values and imported it and used it directly inside the register function.

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,18 @@
-export async function onRegister(event) {}
+import { register } from "../../api/auth/register";
+import { getRegisterFormData } from "../../ui/form/registerFormInput";
+
+export async function onRegister(event) {
+  event.preventDefault();
+
+  const formData = getRegisterFormData();
+
+  try {
+    const response = await register(formData);
+    console.log("Registration successful:", response.data); 
+    alert("Registration successful! " + response.data); 
+    window.location.href = "/auth/login/";
+  } catch (error) {
+    console.error("Registration failed:", error.message);
+    alert(`Registration failed: ${error.message}`);
+  }
+}
