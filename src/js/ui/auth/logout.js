aed11878e269bd121e96485374c5f19603ba855f
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-11 14:58:24 +0200

    add readprofile and readprofiles functions
    Added readprofile function
    added readprofiles function
    
    Readprofile correctly takes in a username and returns the correct user profile data. Added error handling.
    
    Readprofiles function correctly reads all profiles and returns them with a limit and page.
    
    Added documentation JSdocs for the readProfile() and readProfiles()

--- src/js/ui/auth/logout.js
+++ src/js/ui/auth/logout.js
@@ -9,5 +9,5 @@ export function onLogout() {
   localStorage.removeItem("token");
   localStorage.removeItem("user");
   window.location.href = "/auth/login/";
-  console.log("Logout successful");
+  console.log("Logout successful"); // Remove after testing
 }
