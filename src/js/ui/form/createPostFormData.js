1ec1041458e364e59999210eac787ce804e52613
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-16 13:04:23 +0200

    Update createPostFormData.js

--- src/js/ui/form/createPostFormData.js
+++ src/js/ui/form/createPostFormData.js
@@ -1,4 +1,3 @@
-
 /**
  * @description - This function retrieves the data from the create post form inputs.
  * If no media is provided, the media object will be null.
@@ -11,21 +10,21 @@
  * // Output: { title: "", body: "", tags: [], media: { url: "" } }
  */
 export function getCreatePostFormData() {
-    const form = document.getElementById("createPostForm");
-  
-    const title = form.querySelector("#postTitle").value.trim();
-    const body = form.querySelector("#postContent").value.trim() || null;
-  
-    const mediaUrl = form.querySelector("#postImage").value.trim();
-    const mediaAlt = "Post image";
-  
-    const media = mediaUrl ? { url: mediaUrl, alt: mediaAlt } : null;
-  
-    // Get tags values
-    const tags = Array.from(form.querySelectorAll('input[name="tags"]:checked')).map((checkbox) => checkbox.value);
-  
-    // Custom tag for all posts to retrieve only posts by me later
-    tags.push("øyvind"); // <---- Remove this if you want to show all posts
-  
-    return { title, body, tags, media };
-  }
+  const form = document.getElementById("createPostForm");
+
+  const title = form.querySelector("#postTitle").value.trim();
+  const body = form.querySelector("#postContent").value.trim() || null;
+
+  const mediaUrl = form.querySelector("#postImage").value.trim();
+  const mediaAlt = "Post image";
+
+  const media = mediaUrl ? { url: mediaUrl, alt: mediaAlt } : null;
+
+  // Get tags values
+  const tags = Array.from(form.querySelectorAll('input[name="tags"]:checked')).map((checkbox) => checkbox.value);
+
+  // Custom tag for all posts to retrieve only posts by me later
+  tags.push("øyvind"); // <---- Remove this if you want to show all posts
+
+  return { title, body, tags, media };
+}
