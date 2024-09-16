1994aa11579711482805b0accc15300ed9175d2c
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-16 13:02:22 +0200

    fix correct object path

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -4,23 +4,12 @@ import { createPost } from "../../api/post/create";
 export async function onCreatePost(event) {
   event.preventDefault();
 
-  // Get form data
   const formData = getCreatePostFormData();
-
-  // Create post (send form data to API)
   const result = await createPost(formData);
-  console.log("Post created:", result); // Remove after testing
+  console.log("Post created:", result.data); // Remove after testing
 
-  // Make this into a function later on
-  // Create new URL
   const url = new URL(window.location.origin + "/post/");
-
-  // Set the query parameter
   url.searchParams.set("id", result.id);
-
-  // Redirect to the new URL
   window.history.pushState({}, "", url.toString());
   window.location.href = url.toString();
-
-  // Make the query parameter into a function later on
 }
