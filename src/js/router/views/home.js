1e5a5a8a08ac8052555f4ed3e9e5aff5241cfc50
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-16 13:06:21 +0200

    Update home.js
    Created an init function so i can control whats running.

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,7 +1,18 @@
 import { authGuard } from "../../utilities/authGuard";
 import { setLogoutListener } from "../../ui/global/logout";
 import { checkIfPageReload } from "../../utilities/checkIfPageReload";
+import { readPosts } from "../../api/post/read";
+import { loadAndDisplayPosts } from "../../ui/dom/postElements";
+import { displayLoggedInUserHome } from "../../ui/profile/displayUserProfile";
 
-authGuard();
-setLogoutListener();
-checkIfPageReload();// Remove after testing
+
+async function init() {
+    authGuard();
+    setLogoutListener();
+    checkIfPageReload(); // Remove after testing
+    displayLoggedInUserHome();
+    const loadPosts = await readPosts(12,1);
+    loadAndDisplayPosts(loadPosts);
+}
+
+init();
