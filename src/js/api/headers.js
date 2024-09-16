16fcd6c826988fa989dea7c3a8b7b76554e5a0d6
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-16 13:10:57 +0200

    added documentation to headers

--- src/js/api/headers.js
+++ src/js/api/headers.js
@@ -1,5 +1,13 @@
 import { API_KEY } from "./constants";
 
+/**
+ * @description Creates a new Headers object with the necessary headers for making requests to the API.
+ * @returns {Headers} - A Headers object containing the necessary headers for making requests to the API.
+ * @example 
+ * method: "POST",
+ * headers: headers(),
+ * 
+ */
 export function headers() {
   const headers = new Headers();
   headers.append("Content-Type", "application/json");
@@ -12,6 +20,6 @@ export function headers() {
   if (token) {
     headers.append("Authorization", `Bearer ${token}`);
   }
-  
+
   return headers;
 }
