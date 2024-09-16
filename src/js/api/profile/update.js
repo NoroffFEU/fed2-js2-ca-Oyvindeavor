0a6c0d67aa60aad80f0c3c5e4b69384feb0b69de
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-16 13:07:11 +0200

    Update update.js
    fixed the updateProfile function

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,12 +1,19 @@
 import { API_SOCIAL_PROFILES } from "../constants";
+import { headers } from "../headers";
 
-export async function updateProfile(username, { avatar, banner }) {
+export async function updateProfile(username, { avatar, banner, bio }) {
   try {
+    const deconstruct = {
+      avatar: avatar.url,
+      banner: banner.url,
+      bio,
+    };
     const response = await fetch(`${API_SOCIAL_PROFILES}/${username}`, {
       method: "PUT",
       headers: headers(),
-      body: JSON.stringify({ avatar, banner }),
+      body: JSON.stringify(deconstruct),
     });
+    console.log(JSON.stringify(deconstruct)); // Remove after
     const data = await response.json();
     console.log(data); // Remove after testing
     return data;
