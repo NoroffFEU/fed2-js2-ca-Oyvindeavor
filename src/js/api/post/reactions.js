c298ad0a063d6202995b65265eb4de3da15ba4e0
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-19 14:54:30 +0200

    Create reactions.js
    created reactToPost function that takes in the post that you want to react to and an emoji in the second parameter sends a put rquest.

---
+++ src/js/api/post/reactions.js
@@ -0,0 +1,29 @@
+import { API_SOCIAL_POSTS } from "../../api/constants";
+import { headers } from "../headers";
+
+/**
+ * @description Reacts to a post by adding an emoji reaction to it. align-center. Note
+ * that the emoji is a string that represents the emoji. if users react to same post with same emoji, it will be removed.
+ * @param {object} post 
+ * @param {string} emoji 
+ * @returns {Promise<void>} A promise that resolves when the post has been reacted to.
+ * @example
+ * // React to a post with a heart emoji
+ * const post = { id: 1, title: "Example, post", body: "This is an example post.", reactions: 
+ * [{"symbol": "👍","count": 1,"reactors": ["oyvind"]}
+] };
+ * reactToPost(post, "heart")
+ */
+export async function reactToPost(post, emoji) {
+  const url = `${API_SOCIAL_POSTS}/${post.id}/react/${emoji}`;
+  const response = await fetch(url, {
+    method: "PUT",
+    headers: headers(),
+    body: JSON.stringify({}),
+  });
+  const responseData = await response.json();
+  if (!response.ok) {
+    throw new Error(responseData.errors[0].message);
+  }
+  console.log("Reaction data", responseData.data); // remove after testing
+}
