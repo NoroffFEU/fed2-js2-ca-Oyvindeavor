d1c7c9f1028fae72b9b2be6fa99d1bb7bc6032a9
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-19 14:59:13 +0200

    Update create.js
    add some comments

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -4,10 +4,13 @@ import { createPost } from "../../api/post/create";
 export async function onCreatePost(event) {
   event.preventDefault();
 
+  // Gets the form data
   const formData = getCreatePostFormData();
+  // Send api request to create post
   const result = await createPost(formData);
   console.log("Post created:", result.data); // Remove after testing
 
+  // Redirect to the post page after creating the post.
   const url = new URL(window.location.origin + "/post/");
   url.searchParams.set("id", result.id);
   window.history.pushState({}, "", url.toString());
