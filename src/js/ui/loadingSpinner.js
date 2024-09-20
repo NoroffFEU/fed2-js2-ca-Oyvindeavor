8dd756716d780514b947f9afe8cf28a34a4d6734
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-20 08:51:16 +0200

    Delete loadingSpinner.js
    Moved loading spinner to utilities folder

--- src/js/ui/loadingSpinner.js
+++
@@ -1,9 +0,0 @@
-export function showSpinner() {
-  const spinner = document.querySelector(".spinner");
-  spinner.style.display = "block";
-}
-
-export function hideSpinner() {
-  const spinner = document.querySelector(".spinner");
-  spinner.style.display = "none";
-}
