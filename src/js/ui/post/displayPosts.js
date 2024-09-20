34e63b68ef049abdd0258ccb22570fc0109195b0
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-20 08:58:18 +0200

    Update displayPosts.js
    correcting new path

--- src/js/ui/post/displayPosts.js
+++ src/js/ui/post/displayPosts.js
@@ -1,5 +1,5 @@
 import { clearPostsFeed } from "../dom/clear/clearPosts.js";
-import { appendPostToFeed } from "../dom/create/postsElements.js";
+import { appendPostToFeed } from "../dom/create/posts/postsElements.js";
 
 // Clears the posts feed and appends the posts to the feed
 export function loadAndDisplayPosts(posts) {
