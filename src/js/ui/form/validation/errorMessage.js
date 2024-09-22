6b7d3d847616e86f5a91ff6fb1f9c3d92b60d51d
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-22 10:59:18 +0200

    Create errorMessage.js
    Created 3 functions for the handling error messages in forms.

---
+++ src/js/ui/form/validation/errorMessage.js
@@ -0,0 +1,22 @@
+
+
+export function displayErrorMessage(elementId, message) {
+    const errorElement = document.querySelector(`#${elementId}`);
+    errorElement.textContent = message;
+    errorElement.style.display = "block";
+  }
+  
+  
+  export function hideErrorMessage(elementId) {
+    const errorElement = document.querySelector(`#${elementId}`);
+    errorElement.style.display = "none";
+  }
+  
+  export function clearErrorMessages() {
+    const errorElement = document.querySelectorAll(".error-message");
+    errorElement.forEach((element) => {
+      element.textContent = "";
+      element.style.display = "none";
+    });
+  }
+  
