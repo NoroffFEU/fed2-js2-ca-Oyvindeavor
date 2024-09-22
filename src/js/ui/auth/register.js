d1babe7bd1cdd36e653000b15769ce526c92550f
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-22 13:28:43 +0200

    Update register.js

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -8,10 +8,9 @@ export async function onRegister(event) {
   try {
     const response = await register(formData);
     console.log("Registration successful:", response); // Remove after testing
-    alert("Registration successful! Continue to login page "); // Remove after testing
+    alert("Registration successful. Please log in to continue.");
     window.location.href = "/auth/login/";
   } catch (error) {
     console.error("Registration failed:", error.message);
-    alert(`Registration failed: ${error.message}`);
   }
 }
