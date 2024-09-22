7e1a1c0cd3c0c8ef1b87919d0256ce7a004847a3
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-22 11:07:32 +0200

    Update register.js

--- src/js/router/views/register.js
+++ src/js/router/views/register.js
@@ -1,8 +1,10 @@
 import { onRegister } from "../../ui/auth/register";
 import { checkIfPageReload } from "../../utilities/checkIfPageReload";
+import { setFormInputListeners } from "../../ui/form/validation/register";
 
 function init() {
   checkIfPageReload(); // Remove after testing
+  setFormInputListeners();
   const form = document.forms.register;
   form.addEventListener("submit", onRegister);
 }
