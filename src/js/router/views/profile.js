f63bd53df49b3fb9b50ceab1bc6211e1751debed
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-22 11:08:18 +0200

    Update profile.js

--- src/js/router/views/profile.js
+++ src/js/router/views/profile.js
@@ -1,9 +1,11 @@
 import { authGuard } from "../../utilities/authGuard";
 import { checkIfPageReload } from "../../utilities/checkIfPageReload";
+import { setLogoutListener } from "../../ui/global/logout";
 
 function init() {
   authGuard();
   checkIfPageReload(); // Remove after testing
+  setLogoutListener();
 }
 
 init();
