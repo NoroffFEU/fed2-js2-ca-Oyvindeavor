59afcf983a76002b36a67c3a86b8e1072d6cc3af
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-23 17:48:15 +0200

    Update displayUserProfile.js

--- src/js/ui/profile/displayUserProfile.js
+++ src/js/ui/profile/displayUserProfile.js
@@ -5,7 +5,6 @@ export async function displayLoggedInUserHome() {
   console.log(user);
   const username = document.querySelector(".username");
   const avatar = document.querySelector(".avatar-big img");
-
   username.textContent = user.name.charAt(0).toUpperCase() + user.name.slice(1);
   avatar.src = user.avatar.url;
 }
