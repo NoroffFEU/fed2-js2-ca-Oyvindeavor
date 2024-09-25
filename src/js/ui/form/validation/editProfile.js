de6e0bf9eb42c8f37e881263cceab879c94c4efc
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-25 16:32:20 +0200

    Create editProfile.js
    Created a new file to handle the event listeners visual feedback on the update profile form.

---
+++ src/js/ui/form/validation/editProfile.js
@@ -0,0 +1,40 @@
+import { displayErrorMessage, hideErrorMessage } from "../validation/errorMessage";
+
+export function setEditProfileEventListeners() {
+  addAvatarInputListener();
+  addBannerInputListener();
+}
+
+function addAvatarInputListener() {
+  const avatarInput = document.querySelector("#profileImg");
+  avatarInput.addEventListener("input", () => {
+    if (avatarInput.checkValidity()) {
+      avatarInput.style.border = "2px solid green";
+      hideErrorMessage("error-message-profile");
+    } else {
+      avatarInput.style.border = "2px solid red";
+      displayErrorMessage("error-message-profile", "Please enter a valid URL");
+    }
+    if (avatarInput.value === "") {
+      avatarInput.style.border = "";
+      hideErrorMessage("error-message-profile");
+    }
+  });
+}
+
+function addBannerInputListener() {
+  const bannerInput = document.querySelector("#bannerImg");
+  bannerInput.addEventListener("input", () => {
+    if (bannerInput.checkValidity()) {
+      bannerInput.style.border = "2px solid green";
+      hideErrorMessage("error-message-profile");
+    } else {
+      bannerInput.style.border = "2px solid red";
+      displayErrorMessage("error-message-profile", "Please enter a valid URL");
+    }
+    if (bannerInput.value === "") {
+      bannerInput.style.border = "";
+      hideErrorMessage("error-message-profile");
+    }
+  });
+}
