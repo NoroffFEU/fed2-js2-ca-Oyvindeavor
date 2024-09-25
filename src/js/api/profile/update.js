6e5954c9ef4c448a45f71ccd8a055066b534e2d3
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-25 16:24:02 +0200

    Fixed the deconstructuring
    Added if statements to banner and avatar so it only includes them if they are not empty.

--- src/js/api/profile/update.js
+++ src/js/api/profile/update.js
@@ -1,25 +1,25 @@
 import { API_SOCIAL_PROFILES } from "../constants";
 import { headers } from "../headers";
-import {displayErrorMessage} from "../../ui/form/validation/errorMessage";
 
 export async function updateProfile(username, { avatar, banner, bio }) {
   try {
-    const deconstruct = {
-      avatar: { url: avatar },
-      banner: { url: banner },
-      bio,
-    };
+    const deconstruct = { bio };
+    if (avatar) {
+      deconstruct.avatar = { url: avatar };
+    }
+    if (banner) {
+      deconstruct.banner = { url: banner };
+    }
     const response = await fetch(`${API_SOCIAL_PROFILES}/${username}`, {
       method: "PUT",
       headers: headers(),
       body: JSON.stringify(deconstruct),
     });
-    console.log(JSON.stringify(deconstruct)); // Remove after
+    console.log(JSON.stringify(deconstruct)); 
     const data = await response.json();
-    console.log(data); // Remove after testing
+    console.log(data);
     return data;
   } catch (error) {
-    displayErrorMessage("#error-message-profile","Failed to update profile");
     console.error(error);
   }
 }
