6a8125c9ef1d8fc7c8332f710bb8ab4f78e021fa
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-25 16:30:09 +0200

    finished the function
    Finished the onUpdateProfile function
    This now triggers on the submit button on the form of the profile.
    
    Gets the username from the .username element (this is populated on page load)
    
    Calls the getUpdateProfileFormData function this returns the input values from the update profile form.
    
    Calls the updateProfile function
    By passing in name and the form data.  THis sends the put request to the api.
    
    It then updates the ui with the newly created data by calling the readprofile function
    and passing it into the updateProfileDetails function.
    
    In short it updates the profile info with a put request and updates the profile ui to reflect those changes.

--- src/js/ui/profile/update.js
+++ src/js/ui/profile/update.js
@@ -1,1 +1,16 @@
-export async function onUpdateProfile(event) {}
+import { updateProfile } from '../../api/profile/update.js';
+import { updateProfileDetails } from '../dom/create/profile/profileElements.js';
+import { readProfile } from '../../api/profile/read.js';
+import { getUpdateProfileFormData } from '../form/updateProfileInput.js';
+export async function onUpdateProfile(event) {
+    event.preventDefault();
+    const username = document.querySelector(".username").textContent.trim();
+      const profileData = getUpdateProfileFormData();
+
+        // send data to the updateProfile function
+        await updateProfile(username, profileData);
+        // Fetch the updated data from the API
+        const newProfileData = await readProfile(username);
+        // Update the profile details in the ui with the new submitted data
+        updateProfileDetails(newProfileData);
+}
