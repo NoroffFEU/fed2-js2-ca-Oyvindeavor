d1d538f21424104f827999c07a2d6caec6cff65f
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-27 18:21:31 +0200

    Update home.js

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -5,11 +5,13 @@ import { readPosts } from "../../api/post/read";
 import { loadAndDisplayPosts } from "../../ui/post/displayPosts";
 import { displayLoggedInUserHome } from "../../ui/profile/displayUserProfile";
 import { showSpinner, hideSpinner } from "../../utilities/loadingSpinner";
+import { getLoggedInUserName } from "../../ui/profile/getLoggedInUser";
 
-async function init() {
+export async function init() {
   showSpinner();
   authGuard();
   setLogoutListener();
+  setMyProfileEventListener();
   checkIfPageReload(); // Remove after testing
   displayLoggedInUserHome();
   const loadPosts = await readPosts(12, 1, "øyvind");
@@ -18,3 +20,13 @@ async function init() {
 }
 
 init();
+
+
+function setMyProfileEventListener() {
+  const userName = getLoggedInUserName();
+  const myProfileButton = document.querySelector("#myProfileButton");
+  myProfileButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    window.location.href = `/profile/?username=${userName}`;
+  });
+}
