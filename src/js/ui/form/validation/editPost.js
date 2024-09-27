c8c40f7f77c2aa7ce9cb64b27dd9a4c2d86f119e
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-27 15:53:41 +0200

    Create editPost.js
    Created input listeners for the form to handle validation and some visual feedback.

---
+++ src/js/ui/form/validation/editPost.js
@@ -0,0 +1,80 @@
+import { displayErrorMessage, hideErrorMessage } from "./errorMessage";
+
+export function setupPostEditListeners() {
+  addTitleInputListener();
+  addBodyInputListener();
+  addImageInputListener();
+  addTitleBlurEvent();
+  addBodyBlurEvent();
+  addImageBlurEvent();
+}
+
+function addTitleInputListener() {
+  const postTitleInput = document.querySelector("#postTitle");
+  postTitleInput.addEventListener("input", () => {
+    if (postTitleInput.checkValidity()) {
+      postTitleInput.style.border = "2px solid green";
+      hideErrorMessage("error-message-title");
+    } else {
+      postTitleInput.style.border = "2px solid red";
+      displayErrorMessage("error-message-title", "Please enter a title");
+    }
+    if (postTitleInput.value === "") {
+      postTitleInput.style.border = "";
+      displayErrorMessage("error-message-title", "Title is required");
+    }
+  });
+}
+
+function addBodyInputListener() {
+  const postBodyInput = document.querySelector("#postBody");
+  postBodyInput.addEventListener("input", () => {
+    if (postBodyInput.checkValidity()) {
+      postBodyInput.style.border = "2px solid green";
+      hideErrorMessage("error-message-body");
+    }
+  });
+}
+
+function addImageInputListener() {
+  const postImageInput = document.querySelector("#postImage");
+  postImageInput.addEventListener("input", () => {
+    if (postImageInput.checkValidity()) {
+      postImageInput.style.border = "2px solid green";
+      hideErrorMessage("error-message-image");
+    } else {
+      postImageInput.style.border = "2px solid red";
+      displayErrorMessage("error-message-image", "Please enter a valid URL");
+    }
+    if (postImageInput.value === "") {
+      postImageInput.style.border = "";
+      hideErrorMessage("error-message-image");
+    }
+  });
+}
+
+function addTitleBlurEvent() {
+  const postTitle = document.querySelector(".post-title");
+  const postTitleInput = document.querySelector("#postTitle");
+  postTitleInput.addEventListener("blur", () => {
+    postTitle.textContent = postTitleInput.value;
+  });
+}
+
+function addBodyBlurEvent() {
+  const postBody = document.querySelector(".post-body");
+  const postBodyInput = document.querySelector("#postBody");
+  postBodyInput.addEventListener("blur", () => {
+    postBody.textContent = postBodyInput.value;
+  });
+}
+
+function addImageBlurEvent() {
+  const postImage = document.querySelector(".post-img");
+  const postImageInput = document.querySelector("#postImage");
+  postImageInput.addEventListener("blur", () => {
+    if (postImageInput.checkValidity()) {
+      postImage.src = postImageInput.value;
+    }
+  });
+}
