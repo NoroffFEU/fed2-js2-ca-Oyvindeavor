c108b1645c84c1d20191b8e305803bc8da259864
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-28 22:37:18 +0200

    Update home.js

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,6 +1,5 @@
 import { authGuard } from "../../utilities/authGuard";
 import { setLogoutListener } from "../../ui/global/logout";
-import { checkIfPageReload } from "../../utilities/checkIfPageReload";
 import { readPosts } from "../../api/post/read";
 import { loadAndDisplayPosts } from "../../ui/post/displayPosts";
 import { displayLoggedInUserHome } from "../../ui/profile/displayUserProfile";
@@ -13,7 +12,6 @@ export async function init() {
   authGuard();
   setLogoutListener();
   setMyProfileEventListener();
-  checkIfPageReload(); // Remove after testing
   displayLoggedInUserHome();
   const loadPosts = await readPosts(12, 1, "øyvind");
   loadAndDisplayPosts(loadPosts);
