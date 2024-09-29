c18ef5e7c4c1ec21ecde246774f16f17f182464b
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-29 21:47:25 +0200

    Update profile.js

--- src/js/ui/events/profile.js
+++ src/js/ui/events/profile.js
@@ -2,6 +2,7 @@ import { onUpdateProfile } from "../profile/update";
 import { setLogoutListener } from "../global/logout";
 import { setEditProfileEventListeners } from "../form/validation/editProfile";
 
+
 export function setupProfileEventListeners() {
   const form = document.forms.updateProfile;
   form.addEventListener("submit", onUpdateProfile);
