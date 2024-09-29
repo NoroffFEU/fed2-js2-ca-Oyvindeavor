fb3a8e7a6a830e374532e27c7b542a22045b7331
Øyvind <133252875+Oyvindeavor@users.noreply.github.com>
2024-09-29 22:04:21 +0200

    fixed authguard function

--- src/js/utilities/authGuard.js
+++ src/js/utilities/authGuard.js
@@ -1,6 +1,6 @@
 export function authGuard() {
   if (!localStorage.token) {
-    window.location.href = "/auth/";
+    window.location.href = "/auth/login/";
     alert("You must be logged in to view this page");
   }
 }
